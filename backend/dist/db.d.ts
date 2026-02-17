978afdfa36abc855248019a025d16f1c9586f437
Rishabh Damle <dwrushabh@gmail.com>
2026-02-17 14:57:29 +0530

    build for schema

--- backend/dist/db.d.ts
+++ backend/dist/db.d.ts
@@ -1,2 +1,487 @@
-export {};
+import mongoose, { Types } from "mongoose";
+export declare const employeeModel: mongoose.Model<{
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, {}, {}, {
+    id: string;
+}, mongoose.Document<unknown, {}, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, mongoose.DefaultSchemaOptions, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, mongoose.Document<unknown, {}, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, {
+    [path: string]: mongoose.SchemaDefinitionProperty<undefined, any, any>;
+} | {
+    [x: string]: mongoose.SchemaDefinitionProperty<any, any, mongoose.Document<unknown, {}, {
+        name: string;
+        password: string;
+        createdAt: NativeDate;
+        email?: string | null;
+        phoneNumber?: string | null;
+    }, {
+        id: string;
+    }, mongoose.DefaultSchemaOptions> & Omit<{
+        name: string;
+        password: string;
+        createdAt: NativeDate;
+        email?: string | null;
+        phoneNumber?: string | null;
+    } & {
+        _id: Types.ObjectId;
+    } & {
+        __v: number;
+    }, "id"> & {
+        id: string;
+    }> | undefined;
+}, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>;
+export declare const employerModel: mongoose.Model<{
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, {}, {}, {
+    id: string;
+}, mongoose.Document<unknown, {}, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, mongoose.DefaultSchemaOptions, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, mongoose.Document<unknown, {}, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, {
+    [path: string]: mongoose.SchemaDefinitionProperty<undefined, any, any>;
+} | {
+    [x: string]: mongoose.SchemaDefinitionProperty<any, any, mongoose.Document<unknown, {}, {
+        name: string;
+        password: string;
+        createdAt: NativeDate;
+        companyName: string;
+        email?: string | null;
+        phoneNumber?: string | null;
+    }, {
+        id: string;
+    }, mongoose.DefaultSchemaOptions> & Omit<{
+        name: string;
+        password: string;
+        createdAt: NativeDate;
+        companyName: string;
+        email?: string | null;
+        phoneNumber?: string | null;
+    } & {
+        _id: Types.ObjectId;
+    } & {
+        __v: number;
+    }, "id"> & {
+        id: string;
+    }> | undefined;
+}, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>, {
+    name: string;
+    password: string;
+    createdAt: NativeDate;
+    companyName: string;
+    email?: string | null;
+    phoneNumber?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>;
+export declare const adminModel: mongoose.Model<{
+    email: string;
+    password: string;
+}, {}, {}, {
+    id: string;
+}, mongoose.Document<unknown, {}, {
+    email: string;
+    password: string;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    email: string;
+    password: string;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, mongoose.DefaultSchemaOptions, {
+    email: string;
+    password: string;
+}, mongoose.Document<unknown, {}, {
+    email: string;
+    password: string;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    email: string;
+    password: string;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, {
+    [path: string]: mongoose.SchemaDefinitionProperty<undefined, any, any>;
+} | {
+    [x: string]: mongoose.SchemaDefinitionProperty<any, any, mongoose.Document<unknown, {}, {
+        email: string;
+        password: string;
+    }, {
+        id: string;
+    }, mongoose.DefaultSchemaOptions> & Omit<{
+        email: string;
+        password: string;
+    } & {
+        _id: Types.ObjectId;
+    } & {
+        __v: number;
+    }, "id"> & {
+        id: string;
+    }> | undefined;
+}, {
+    email: string;
+    password: string;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>, {
+    email: string;
+    password: string;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>;
+export declare const jobModel: mongoose.Model<{
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+}, {}, {}, {
+    id: string;
+}, mongoose.Document<unknown, {}, {
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, mongoose.DefaultSchemaOptions, {
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+}, mongoose.Document<unknown, {}, {
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, {
+    [path: string]: mongoose.SchemaDefinitionProperty<undefined, any, any>;
+} | {
+    [x: string]: mongoose.SchemaDefinitionProperty<any, any, mongoose.Document<unknown, {}, {
+        createdAt: NativeDate;
+        description: string;
+        title: string;
+        employerId: Types.ObjectId;
+        location?: string | null;
+        salary?: number | null;
+        industry?: string | null;
+    }, {
+        id: string;
+    }, mongoose.DefaultSchemaOptions> & Omit<{
+        createdAt: NativeDate;
+        description: string;
+        title: string;
+        employerId: Types.ObjectId;
+        location?: string | null;
+        salary?: number | null;
+        industry?: string | null;
+    } & {
+        _id: Types.ObjectId;
+    } & {
+        __v: number;
+    }, "id"> & {
+        id: string;
+    }> | undefined;
+}, {
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>, {
+    createdAt: NativeDate;
+    description: string;
+    title: string;
+    employerId: Types.ObjectId;
+    location?: string | null;
+    salary?: number | null;
+    industry?: string | null;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>;
+export declare const applicationModel: mongoose.Model<{
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+}, {}, {}, {
+    id: string;
+}, mongoose.Document<unknown, {}, {
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any, any>, {}, {}, {}, {}, mongoose.DefaultSchemaOptions, {
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+}, mongoose.Document<unknown, {}, {
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+}, {
+    id: string;
+}, mongoose.DefaultSchemaOptions> & Omit<{
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}, "id"> & {
+    id: string;
+}, {
+    [path: string]: mongoose.SchemaDefinitionProperty<undefined, any, any>;
+} | {
+    [x: string]: mongoose.SchemaDefinitionProperty<any, any, mongoose.Document<unknown, {}, {
+        employeeId: Types.ObjectId;
+        jobId: Types.ObjectId;
+        status: string;
+        appliedAt: NativeDate;
+    }, {
+        id: string;
+    }, mongoose.DefaultSchemaOptions> & Omit<{
+        employeeId: Types.ObjectId;
+        jobId: Types.ObjectId;
+        status: string;
+        appliedAt: NativeDate;
+    } & {
+        _id: Types.ObjectId;
+    } & {
+        __v: number;
+    }, "id"> & {
+        id: string;
+    }> | undefined;
+}, {
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>, {
+    employeeId: Types.ObjectId;
+    jobId: Types.ObjectId;
+    status: string;
+    appliedAt: NativeDate;
+} & {
+    _id: Types.ObjectId;
+} & {
+    __v: number;
+}>;
 //# sourceMappingURL=db.d.ts.map
--- backend/dist/db.js
+++ backend/dist/db.js
@@ -16,4 +16,62 @@ if (DB_URL) {
 else {
     console.error("DB_URL is not set; skipping Mongo connection");
 }
+import { model, Schema } from "mongoose";
+const employeeSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true },
+    phoneNumber: { type: String, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+export const employeeModel = model("Employee", employeeSchema);
+const employerSchema = new Schema({
+    companyName: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, unique: true },
+    phoneNumber: { type: String, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+export const employerModel = model("Employer", employerSchema);
+const adminSchema = new Schema({
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+});
+export const adminModel = model("Admin", adminSchema);
+const jobSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    location: String,
+    salary: Number,
+    industry: String,
+    employerId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employer",
+        required: true,
+    },
+    createdAt: { type: Date, default: Date.now },
+});
+export const jobModel = model("Job", jobSchema);
+const applicationSchema = new Schema({
+    employeeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Employee",
+        required: true,
+    },
+    jobId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+        required: true,
+    },
+    status: {
+        type: String,
+        default: "applied",
+    },
+    appliedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+export const applicationModel = model("Application", applicationSchema);
 //# sourceMappingURL=db.js.map
