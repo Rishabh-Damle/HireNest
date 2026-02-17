b68d8dc8a83392938bf7558bf67bd0d419c674bd
Rishabh Damle <dwrushabh@gmail.com>
2026-02-17 13:36:47 +0530

    DB setup complete

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,12 +4,13 @@ const app = express();
 import { adminRouter } from "./routes/admin.js";
 import { employeeRouter } from "./routes/employee.js";
 import { employerRouter } from "./routes/employer.js";
+import { PORT } from "./config.js";
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/employee", employeeRouter);
 app.use("/api/v1/employer", employerRouter);
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is started on port 3000");
 });
 //# sourceMappingURL=index.js.map
